4cf94470e4e0fee921a754dff912b281c6d50dba
Felipe Santos <carlosfsp@dcc.ufrj.br>
2025-02-21 19:08:28 -0300

    model task criado

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Task(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 # Create your models here.
