9cc592de1a561bd327b64ab7a72b5d5ba0e6c31a
Felipe Santos <carlosfsp@dcc.ufrj.br>
2025-02-21 21:03:38 -0300

    registro, login e autenticação via token de usuario

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
